1baf926bdd7502c573e239a7034cd69c836d31f6
kseckman <kseckm1@gmail.com>
2025-03-04 17:26:59 -0500

    cleaned up files, css update

--- main.js
+++ main.js
@@ -3,25 +3,25 @@
 // function that keeps track of player turn
 // function that places a mark for a player
 // function to check game over after each round
-//read up on factory functions/ iife
-
-//create and render board use iife
+// read up on factory functions/ iife
+// create and render board use iife
 const Gameboard = (()=>{
-    let gameboard = ['','','','','','','','','']
+    let gameboard = ['','','','','','','','',''];
     const createBoard= ()=>{
         const board = document.querySelector('.gameboard')
+        board.classList.toggle('background')
         gameboard.forEach((box, index)=>{
-            const divEl = document.createElement('div')
-            divEl.classList.add('square')
-            divEl.id = index
-            divEl.textContent = `${box}`
-            divEl.addEventListener('click', Game.handleClick)
-            board.append(divEl)
+            const divEl = document.createElement('div');
+            divEl.classList.add('square');
+            divEl.id = index;
+            divEl.textContent = `${box}`;
+            divEl.addEventListener('click', Game.handleClick);
+            board.append(divEl);
         })
     }
 
     const update = (index, mark)=>{
-        gameboard[index] = mark
+        gameboard[index] = mark;
     }
 
     const getGameboard = ()=> gameboard
@@ -37,7 +37,7 @@ const createPlayer = (name, mark)=>{
 }
 
 const Game =(()=>{
-    let players = []
+    let players = [];
     let currentPlayer;
     let gameOver;
 
@@ -48,20 +48,19 @@ const Game =(()=>{
             createPlayer(playerOne.value = 'Player One', 'X'),
             createPlayer(playerTwo.value = 'Player Two', 'O')
         ]
-        document.querySelector('#player1-display').textContent = playerOne.value
-        document.querySelector('#player2-display').textContent = playerTwo.value
+        document.querySelector('#player1-display').textContent = playerOne.value;
+        document.querySelector('#player2-display').textContent = playerTwo.value;
         currentPlayer = 0;
         gameOver= false;
         if(!document.querySelector('.square')) return Gameboard.createBoard();
     }
-    //click events . here we update, check for win, switch player
-    const handleClick = (e)=>{
-        if(gameOver) return
-        let index = +e.target.id
-        if(Gameboard.getGameboard()[index] !== '') return
-        e.target.textContent = players[currentPlayer].mark
 
-        Gameboard.update(index, players[currentPlayer].mark)
+    const handleClick = (e)=>{
+        if(gameOver) return;
+        let index = +e.target.id;
+        if(Gameboard.getGameboard()[index] !== '') return;
+        e.target.textContent = players[currentPlayer].mark;
+        Gameboard.update(index, players[currentPlayer].mark);
 
         if(checkForWin(Gameboard.getGameboard(), players[currentPlayer].mark)){
             gameOver = true;
@@ -73,20 +72,18 @@ const Game =(()=>{
             results.textContent = `It's a tie!`
         }
         
-        currentPlayer = currentPlayer === 0? 1 : 0
-        console.log(currentPlayer, index, players[currentPlayer].mark)
+        currentPlayer = currentPlayer === 0? 1 : 0;
     }
     
-    //restart
     const reset = ()=>{
         gameOver = false;
         const squares = document.querySelectorAll('.square')
         for(let i=0; i < squares.length; i++){
             Gameboard.update(i, '')
-            squares[i].textContent = ''
+            squares[i].textContent = '';
         }
         const results = document.querySelector('.results-display')
-        results.textContent = ''
+        results.textContent = '';
     }
     return {
         start,
@@ -95,7 +92,6 @@ const Game =(()=>{
     }
 })()
 
-//winning logic
 const checkForWin = (board)=>{
     const winningCombinations= [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
@@ -103,19 +99,18 @@ const checkForWin = (board)=>{
             [0, 4, 8], [2, 4, 6]  
         ]
     for(let i=0; i < winningCombinations.length; i++){
-        const [a,b,c] = winningCombinations[i]
+        const [a,b,c] = winningCombinations[i];
         if(board[a] && board[a] === board[b] && board[a] === board[c]){
-            return true
+            return true;
         }
     }
-    return false
+    return false;
 }
 
 const checkForTie = (board)=>{
    return board.every(cell=> cell !== '')
 }
 
-//click events for buttons
 const resetBtn = document.querySelector('#restart-button')
 resetBtn.addEventListener('click',()=>{
     Game.reset()
